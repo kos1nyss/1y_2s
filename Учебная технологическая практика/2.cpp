2c612f687581da8546f4f957bf1eee13927c4961
kos1nyss <44138438+kos1nyss@users.noreply.github.com>
2023-01-19 17:35:29 +0200

    + 2я практика

---
+++ Учебная технологическая практика/2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int N = 1000;
+	int mas_a[N] = { 1, 2, 3, 4, 5 };
+	int mas_b[N] = { 0 };
+
+	const char* f_pointer = (const char*)mas_a;
+	char* t_pointer = (char*)mas_b;
+	for (int i = 0; i < N; ++i)
+	{
+		*(t_pointer++) = *(f_pointer++);
+	}
+}
