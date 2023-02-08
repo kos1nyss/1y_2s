91122cf3ff4317985176245f6d4f2fc3cfda5048
kos1nyss <44138438+kos1nyss@users.noreply.github.com>
2023-02-08 20:00:26 +0200

    + 3я лабораторная

---
+++ Программирование/3.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+
+class Complex {
+private:
+    double a, b;
+
+public:
+    Complex(double a, double b) {
+        this->a = a;
+        this->b = b;
+    }
+
+    Complex(const Complex& other) {
+        a = other.a;
+        b = other.b;
+    }
+
+    Complex& operator = (const Complex& other) {
+        a = other.a;
+        b = other.b;
+        return *this;
+    }
+
+    Complex& operator += (const Complex& other) {
+        a += other.a;
+        b += other.b;
+        return *this;
+    }
+
+    Complex& operator *= (const Complex& other) {
+        a = a * other.a - b * other.b;
+        b = a * other.b + other.a * b;
+        return *this;
+}
+
+Complex& operator * (const Complex& other) {
+    Complex new_complex = Complex(a * other.a - b * other.b, a * other.b + other.a * b);
+    return new_complex;
+}
+
+Complex& operator ++ () {
+    ++a;
+    return *this;
+}
+
+Complex& operator ++ (int _) {
+    a++;
+    return *this;
+}
+
+friend std::ostream& operator << (std::ostream& os, const Complex& c);
+friend std::istream& operator >> (std::istream& in, Complex& c);
+};
+
+
+std::ostream& operator << (std::ostream& os, const Complex& c) {
+    os << c.a << " + " << c.b << 'i';
+    return os;
+}
+
+std::istream& operator >> (std::istream& in, Complex& c) {
+    in >> c.a;
+    in >> c.b;
+    return in;
+}
